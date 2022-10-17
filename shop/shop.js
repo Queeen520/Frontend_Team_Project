234e8520425151528a8234f421015b5c9fe8e83b
iamwahida <nemati.w@yahoo.com>
2022-10-17 14:16:16 +0200

    next

--- shop/shop.js
+++ shop/shop.js
@@ -1,1 +1,41 @@
-console.log("hello");
+
+//created properties with values
+let items = [
+    {
+        name: "The shark",
+        description: "etcetcetc",
+        img: "hai.jpg",
+        price: 23
+    },
+    {
+        name: "The ocean",
+        description: "etcetcetc",
+        img: "ocean.jpg",
+        price: 23
+    },
+    {
+        name: "The jellyfish",
+        description: "etcetcetc",
+        img: "qualle.jpg",
+        price: 24
+    },
+    {
+        name: "The white bird",
+        description: "etcetcetc",
+        img: "vogel.jpg",
+        price: 24
+    },
+    {
+        name: "The desert",
+        description: "etcetcetc",
+        img: "wueste.jpg",
+        price: 24
+    },
+    {
+        name: "The wind turbine",
+        description: "Stalled wind turbines cost hundreds of millions of euros.",
+        img: "windkraft.jpg",
+        price: 24
+    }
+]
+//print the items
