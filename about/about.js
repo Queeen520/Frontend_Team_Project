ec63b77949d18fb706648c5126a3916b06c39cc0
Marko <marko.dzomba.info@gmail.com>
2022-10-18 12:43:52 +0200

    navbar

--- about/about.js
+++ about/about.js
@@ -1,3 +1,21 @@
+
+var navbar = document.getElementById("mainNavigation");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
 document.getElementById('box').addEventListener('click', sendEmail);
 
 
@@ -40,5 +58,7 @@ function sendEmail() {
     }
 
 }
+
+
 
 
